bd8421e75d0680cea44033c77133cadc92c31820
Sagar Rai <sagarrai9893@gmail.com>
2026-01-12 13:20:34 +0530

    Initial Commit

---
+++ doit/doitApp.swift
@@ -0,0 +1,17 @@
+//
+//  doitApp.swift
+//  doit
+//
+//  Created by Apple on 12/01/26.
+//
+
+import SwiftUI
+
+@main
+struct doitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
